b1cfa8d90607b766773caea2d49aaa8f1b122443
Jaytun Kankotiya <jaytunkankotiya81@gmail.com>
2025-11-04 00:23:36 -0500

    Updated Version

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -66,9 +66,10 @@ export const googleAuth = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
+
     res.status(200).json({ message: "Login successful", user });
   } catch (error) {
     console.error("Google OAuth error:", error.message);
